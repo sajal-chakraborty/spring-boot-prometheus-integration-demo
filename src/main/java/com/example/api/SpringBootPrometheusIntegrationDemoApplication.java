f8813fb62ef93e63cb91521072afde9c3b52645b
“Sajal <sajal.chakraborty@gmail.com>
2026-03-29 22:33:34 +0530

    Initial Checkin

---
+++ src/main/java/com/example/api/SpringBootPrometheusIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrometheusIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrometheusIntegrationDemoApplication.class, args);
+	}
+
+}
